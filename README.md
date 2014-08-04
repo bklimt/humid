c79fd419792123de21983d84f68c20694ff8dd3f
Bryan Klimt <bklimt@gmail.com>
2014-08-03 17:40:39 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+Humid
+=====
+
+A go app for controlling Philips Hue lightbulbs with a MIDI controller.
+Specifically designed for use on the Raspberry Pi.
+
+# Setup
+
+Depends on my [MIDI package](https://github.com/bklimt/midi) and my [Hue package](https://github.com/bklimt/hue), so make sure to install the dependencies for those. Specifically, you'll need to make sure you install the ALSA asound development libraries.
+
+You'll need to make sure your Hue is configured correctly. Build the `hue` command-line tool.
+
+    go install github.com/bklimt/hue/...
+
+Then press the link button on your Hue router and use the command-line tool to register the app.
+
+    $GOPATH/bin/hue --ip="YOUR.ROUTER.IP.ADDRESS" --register
+
+Once that it complete, you can start the `humid` app to listen for MIDI events to control the lights.
+
+    $GOPATH/bin/humid --ip="YOUR.ROUTER.IP.ADDRESS"
+
+While `humid` is running, you can see it as a MIDI receiver on your system by running:
+
+    aconnect -loi
+
+You'll need to use `aconnect` to bind the MIDI controller you want to use to `humid`, something like:
+
+    aconnect 20 128
+
+To change how the controller inputs map to lighting values, edit `presets.json`.
---
+++ cmd/humid/humid.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+  "encoding/json"
+  "flag"
+	"fmt"
+  "github.com/bklimt/hue"
+	"github.com/bklimt/midi"
+  "io/ioutil"
+  "strconv"
+)
+
+type PresetMap map[string]map[string]hue.LightRequestBody
+
+func midiOn(note int, lights *hue.Hue, presets *PresetMap) {
+  if lightMap, ok := (*presets)[strconv.Itoa(note)]; ok {
+    for light, req := range lightMap {
+      lights.ChangeLight(light, &req)
+    }
+  }
+}
+
+func main() {
+	ip := flag.String("ip", "192.168.1.3", "IP Address of Philips Hue hub.")
+	userName := flag.String("username", "HueGoRaspberryPiUser", "Username for Hue hub.")
+	deviceType := flag.String("device_type", "HueGoRaspberryPi", "Device type for Hue hub.")
+
+  presetsFile := flag.String("presets", "./presets.json", "Presets file to use.")
+
+  flag.Parse()
+
+	lights := &hue.Hue{*ip, *userName, *deviceType}
+
+  b, err := ioutil.ReadFile(*presetsFile)
+  if err != nil {
+    fmt.Printf("Unable to open presets file: %v\n", err)
+    return
+  }
+
+  var presets PresetMap
+  err = json.Unmarshal(b, &presets)
+  if err != nil {
+    fmt.Printf("Unable to parse presets file: %v\n", err)
+    return
+  }
+
+  fmt.Printf("Presets: %v", presets)
+
+	c := make(chan interface{})
+	midi.Listen(c)
+	for event := range c {
+		switch event := event.(type) {
+		case midi.Controller:
+			fmt.Printf("Controller event: %d %d\n", event.Param, event.Value)
+		case midi.NoteOn:
+			fmt.Printf("Note on: %d\n", event.Note)
+      midiOn(event.Note, lights, &presets)
+		case midi.NoteOff:
+			fmt.Printf("Note off: %d\n", event.Note)
+		}
+	}
+}
