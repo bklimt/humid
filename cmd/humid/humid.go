ff079c9f5903afdc11800bcd15593b89dea5fa7a
Bryan Klimt <bklimt@gmail.com>
2014-08-20 01:32:14 -0700

    Add support for midi controls.

--- cmd/humid/humid.go
+++ cmd/humid/humid.go
@@ -10,12 +10,43 @@ import (
 	"strconv"
 )
 
-type PresetMap map[string]map[string]hue.LightRequestBody
+type PresetMap struct {
+	Controls map[string]map[string][]string
+	Notes    map[string]map[string]hue.LightRequestBody `json:"notes"`
+}
 
 func midiOn(note int, lights *hue.Hue, presets *PresetMap) {
-	if lightMap, ok := (*presets)[strconv.Itoa(note)]; ok {
+	if lightMap, ok := presets.Notes[strconv.Itoa(note)]; ok {
 		for light, req := range lightMap {
 			lights.ChangeLight(light, &req)
+		}
+	}
+}
+
+func midiControl(param, value int, lights *hue.Hue, presets *PresetMap) {
+	if lightMap, ok := presets.Controls[strconv.Itoa(param)]; ok {
+		for light, attrs := range lightMap {
+			req := &hue.LightRequestBody{}
+			for _, attr := range attrs {
+				switch attr {
+				case "on":
+					t := true
+					req.On = &t
+				case "off":
+					f := false
+					req.On = &f
+				case "bri":
+					bri := value * 2
+					req.Bri = &bri
+				case "sat":
+					sat := value * 2
+					req.Sat = &sat
+				case "hue":
+					hue := (value * 2) << 8
+					req.Hue = &hue
+				}
+			}
+			lights.ChangeLight(light, req)
 		}
 	}
 }
@@ -52,6 +83,7 @@ func main() {
 		switch event := event.(type) {
 		case midi.Controller:
 			fmt.Printf("Controller event: %d %d\n", event.Param, event.Value)
+			midiControl(event.Param, event.Value, lights, &presets)
 		case midi.NoteOn:
 			fmt.Printf("Note on: %d\n", event.Note)
 			midiOn(event.Note, lights, &presets)
