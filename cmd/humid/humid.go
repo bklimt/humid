bcf399b12057b5fd95a94afac0b97ba4be5c7021
Bryan Klimt <bklimt@gmail.com>
2014-08-21 05:24:55 -0700

    Throttle http requests and merge ops.

--- cmd/humid/humid.go
+++ cmd/humid/humid.go
@@ -7,23 +7,98 @@ import (
 	"github.com/bklimt/hue"
 	"github.com/bklimt/midi"
 	"io/ioutil"
+	"log"
 	"strconv"
 )
 
-type PresetMap struct {
+var lights hue.Hue
+
+var presets struct {
 	Controls map[string]map[string][]string
 	Notes    map[string]map[string]hue.PutLightRequest `json:"notes"`
 }
 
-func midiOn(note int, lights *hue.Hue, presets *PresetMap) {
+// Coalesced operations for a single light in one network request.
+type lightOp struct {
+	Light string
+	Op    hue.PutLightRequest
+	Next  *lightOp
+}
+
+// A queue of all the light operations to do over the network.
+type lightOpQueue struct {
+	Head *lightOp
+	Tail *lightOp
+}
+
+var q lightOpQueue
+
+func (q *lightOpQueue) pushBack(op lightOp) {
+	op.Next = nil
+	if q.Tail == nil {
+		q.Head = &op
+		q.Tail = &op
+	} else {
+		q.Tail.Next = &op
+		q.Tail = q.Tail.Next
+	}
+}
+
+func (q *lightOpQueue) popFront() (lightOp, bool) {
+	if q.Head == nil {
+		return lightOp{}, false
+	} else {
+		e := q.Head
+		q.Head = q.Head.Next
+		if q.Tail == e {
+			q.Tail = nil
+		}
+		return *e, true
+	}
+}
+
+func processLightRequests(c chan lightOp) {
+	for lop := range c {
+		log.Printf("Updating light %v with: %v", lop.Light, lop.Op)
+		lights.PutLight(lop.Light, &lop.Op)
+	}
+}
+
+func enqueue(op lightOp) {
+	for p := q.Head; p != nil; p = p.Next {
+		if op.Light == p.Light {
+			// Merge the two ops.
+			log.Printf("Merging op into previous unfinished op: %v <- %v", p, op)
+			if op.Op.On != nil {
+				p.Op.On = op.Op.On
+			}
+			if op.Op.Hue != nil {
+				p.Op.Hue = op.Op.Hue
+			}
+			if op.Op.Sat != nil {
+				p.Op.Sat = op.Op.Sat
+			}
+			if op.Op.Bri != nil {
+				p.Op.Bri = op.Op.Bri
+			}
+			log.Printf("Result: %v", p)
+			return
+		}
+	}
+	// It wasn't in the queue, so add it at the end.
+	log.Printf("Enqueuing operation: %v", op)
+	q.pushBack(op)
+}
+
+func midiOn(note int) {
 	if lightMap, ok := presets.Notes[strconv.Itoa(note)]; ok {
 		for light, req := range lightMap {
-			lights.PutLight(light, &req)
+			enqueue(lightOp{light, req, nil})
 		}
 	}
 }
 
-func midiControl(param, value int, lights *hue.Hue, presets *PresetMap) {
+func midiControl(param, value int) {
 	if lightMap, ok := presets.Controls[strconv.Itoa(param)]; ok {
 		for light, attrs := range lightMap {
 			req := &hue.PutLightRequest{}
@@ -46,9 +121,38 @@ func midiControl(param, value int, lights *hue.Hue, presets *PresetMap) {
 					req.Hue = &hue
 				}
 			}
-			lights.PutLight(light, req)
+			enqueue(lightOp{light, *req, nil})
 		}
 	}
+}
+
+func midiEvent(event interface{}) {
+	switch event := event.(type) {
+	case midi.Controller:
+		fmt.Printf("Controller event: %d %d\n", event.Param, event.Value)
+		midiControl(event.Param, event.Value)
+	case midi.NoteOn:
+		fmt.Printf("Note on: %d\n", event.Note)
+		midiOn(event.Note)
+	case midi.NoteOff:
+		fmt.Printf("Note off: %d\n", event.Note)
+	}
+}
+
+func loadPresets(filename string) {
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("Unable to open presets file: %v\n", err)
+		return
+	}
+
+	err = json.Unmarshal(b, &presets)
+	if err != nil {
+		fmt.Printf("Unable to parse presets file: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Presets: %v", presets)
 }
 
 func main() {
@@ -60,35 +164,29 @@ func main() {
 
 	flag.Parse()
 
-	lights := &hue.Hue{*ip, *userName, *deviceType}
+	lights = hue.Hue{*ip, *userName, *deviceType}
 
-	b, err := ioutil.ReadFile(*presetsFile)
-	if err != nil {
-		fmt.Printf("Unable to open presets file: %v\n", err)
-		return
-	}
+	loadPresets(*presetsFile)
 
-	var presets PresetMap
-	err = json.Unmarshal(b, &presets)
-	if err != nil {
-		fmt.Printf("Unable to parse presets file: %v\n", err)
-		return
-	}
+	midiChan := make(chan interface{})
+	midi.Listen(midiChan)
 
-	fmt.Printf("Presets: %v", presets)
+	httpChan := make(chan lightOp)
+	go processLightRequests(httpChan)
 
-	c := make(chan interface{})
-	midi.Listen(c)
-	for event := range c {
-		switch event := event.(type) {
-		case midi.Controller:
-			fmt.Printf("Controller event: %d %d\n", event.Param, event.Value)
-			midiControl(event.Param, event.Value, lights, &presets)
-		case midi.NoteOn:
-			fmt.Printf("Note on: %d\n", event.Note)
-			midiOn(event.Note, lights, &presets)
-		case midi.NoteOff:
-			fmt.Printf("Note off: %d\n", event.Note)
+	for {
+		if q.Head == nil {
+			// If the queue is empty just wait on a midi event and put it in the queue.
+			midiEvent(<-midiChan)
+		} else {
+			// If the queue has stuff, then prepare an op and wait for either.
+			op := q.Head
+			select {
+			case event := <-midiChan:
+				midiEvent(event)
+			case httpChan <- *op:
+				q.popFront()
+			}
 		}
 	}
 }
